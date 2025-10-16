52b88726978d72f4ac6a5c107cee79becdb42038
Yohei-tazawa1023 <dq.sr.1996@gmail.com>
2025-10-16 12:10:38 +0900

    コミット

--- js/main.js
+++ js/main.js
@@ -16,3 +16,30 @@ window.addEventListener('scroll', function () {
     }
   });
 });
+
+window.addEventListener('load', function () {
+  const scroll = window.scrollY; // スクロール量を取得
+  const windowHeight = window.innerHeight; // 画面の高さを取得
+  const boxes = document.querySelectorAll('.is-fadeIn'); // すべての.is-fadeInを取得
+  const fadeInPosition = -200; // フェードインの位置を設定
+  boxes.forEach(function (box) {
+    const distanceToBox = box.offsetTop;
+    if (scroll + windowHeight + fadeInPosition > distanceToBox) {
+      box.classList.add('is-active');
+    }
+  });
+});
+
+// スムーススクロール
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 60,
+        behavior: "smooth"
+      });
+    }
+  });
+});
